b01f1ac23a039fd33a19845eb739300da7c87710
Aaron Harabedian <aharabed@uncc.edu>
2019-04-15 01:43:36 +0000

    Added create and show to card deck

--- app/controllers/card_decks_controller.rb
+++ app/controllers/card_decks_controller.rb
@@ -2,7 +2,20 @@ class CardDecksController < ApplicationController
     def new
     end
     
-    def create
+    def show
+        @cardDeck = CardDeck.find(params[:id])
     end
+    
+    def create
+        @cardDeck = CardDeck.new(cardDeck_params)
+        
+        @cardDeck.save
+        redirect_to @cardDeck
+    end
+    
+    private
+        def cardDeck_params
+            params.require(:CardDeck).permit(:name)
+        end
 end
 
