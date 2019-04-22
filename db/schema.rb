32f9492cb3050789ca4d69951238e2efebce6e91
Aaron Harabedian <aharabed@uncc.edu>
2019-04-22 19:24:37 +0000

    Added secure password to user and to DB. Added bcrypt to gem file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_191952) do
+ActiveRecord::Schema.define(version: 2019_04_22_192321) do
 
   create_table "card_decks", force: :cascade do |t|
     t.string "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_04_22_191952) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
